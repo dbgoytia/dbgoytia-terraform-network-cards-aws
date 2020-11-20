b9ef21c9d7ff07d275a86ba1ada26b9f5dc9544a
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-20 14:52:19 -0600

    Fixed output for dynamic provisioning..

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "VPC_ID" {
 output "INSTANCE_IP" {
   value = module.instances.INSTANCE_PUBLIC_IP
 }
+
+# Output the public IP's of the worker nodes
+output "NODES_PUBLIC_IP" {
+    value = {
+        for instance in aws_instance.webserver:
+            instance.id => instance.public_ip
+    }
+}
