b204164a063346c9b1caceb43c7f0b9cbf27505d
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-20 13:21:41 -0600

    Forgot to add the key-pair name

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "instances" {
   source        = "git@github.com:dbgoytia/instances-tf.git"
   instance-type = "t2.micro"
   ssh-key-arn = "arn:aws:secretsmanager:us-east-1:779136181681:secret:dgoytia-ssh-key-2-6JJZH2"
-  #key_pair_name = "dgoytia"
+  key_pair_name = "dgoytia"
   #key_pair_path = "~/.ssh/id_rsa.pub" ## I suspect this ain't gonna work.
   vpc_id        = module.network.VPC_ID
   subnet_id     = module.network.SUBNET_ID
