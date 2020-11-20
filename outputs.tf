903874f2c338209e0f270978a3da715a31693aa3
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-20 14:59:04 -0600

    trying something new..

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,10 @@ output "VPC_ID" {
   value = module.network.VPC_ID
 }
 
-# output "INSTANCE_IP" {
-#   value = module.instances.INSTANCE_PUBLIC_IP
-# }
-
 # Output the public IP's of the worker nodes
 output "NODES_PUBLIC_IP" {
     value = {
-        for instance in aws_instance.webserver:
+        for instance in module.instances.NODES_IDS:
             instance.id => instance.public_ip
     }
 }
