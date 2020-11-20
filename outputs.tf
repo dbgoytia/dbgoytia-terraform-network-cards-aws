b153c4cb9c4c1715817a6cc67d6dd9fc6c793403
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-20 15:05:09 -0600

    output fixed

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@ output "VPC_ID" {
 }
 
 # Output the public IP's of the worker nodes
-output "NODES_PUBLIC_IP" {
+output "NODES_PUBLIC_IPS" {
     value = {
-        for instance in module.instances.NODES_IDS:
+        for instance in module.instances.NODES_PUBLIC_IPS:
             instance.id => instance.public_ip
     }
 }
