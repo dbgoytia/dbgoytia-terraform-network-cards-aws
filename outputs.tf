ffdeaeed670af261a0d60b5622c2739c03a00ed2
Diego Goytia <diego.canizales1995@gmail.com>
2020-11-20 14:55:47 -0600

    fixed outputs to match modules.

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "VPC_ID" {
   value = module.network.VPC_ID
 }
 
-output "INSTANCE_IP" {
-  value = module.instances.INSTANCE_PUBLIC_IP
-}
+# output "INSTANCE_IP" {
+#   value = module.instances.INSTANCE_PUBLIC_IP
+# }
 
 # Output the public IP's of the worker nodes
 output "NODES_PUBLIC_IP" {
